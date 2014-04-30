79a6824a990e5696fe77ddae39a1caf5c2424434
Nikolai <n.oreshkin@gmail.comgit config --global user.email n.oreshkin@gmail.comgit config --global user.email n.oreshkin@gmail.com>
2014-04-30 15:13:55 -0700

    new php

--- sendmail.php
+++ sendmail.php
@@ -7,47 +7,73 @@ ini_set('display_errors', 1);
 require_once('phpmail/class.phpmailer.php');
 //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
-$mail             = new PHPMailer();
+// $mail             = new PHPMailer();
 
-$body = (
-  'У чела имэйл <strong>'.
-  $_POST['email'].
-  $_POST['fname']. 
-  '</strong> и он оставил мессагу<hr>'.
-  $_POST['message']
-);
+// $body = (
+//   'У чела имэйл <strong>'.
+//   $_POST['email'].
+//   $_POST['fname']. 
+//   '</strong> и он оставил мессагу<hr>'.
+//   $_POST['message']
+// );
 
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "smtp.gmail.com"; // SMTP server
-$mail->SMTPDebug  = 1;                     // enables SMTP debug information (for testing)
-                                           // 1 = errors and messages
-                                           // 2 = messages only
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->SMTPSecure = "tls";                 // sets the prefix to the servier
-$mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-$mail->Port       = 465;                   // set the SMTP port for the GMAIL server
-$mail->Username   = "zeneronlabs@gmail.com";  // GMAIL username
-$mail->Password   = "5comrapo";            // GMAIL password
+// $mail->IsSMTP(); // telling the class to use SMTP
+// $mail->Host       = "smtp.gmail.com"; // SMTP server
+// $mail->SMTPDebug  = 1;                     // enables SMTP debug information (for testing)
+//                                            // 1 = errors and messages
+//                                            // 2 = messages only
+// $mail->SMTPAuth   = true;                  // enable SMTP authentication
+// $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
+// $mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+// $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
+// $mail->Username   = "zeneronlabs@gmail.com";  // GMAIL username
+// $mail->Password   = "5comrapo";            // GMAIL password
 
-$mail->SetFrom("zeneronlabs@gmail.com");
+// $mail->SetFrom("zeneronlabs@gmail.com");
 
-$mail->AddReplyTo($_POST['email'],  $_POST['fname']);
+// $mail->AddReplyTo($_POST['email'],  $_POST['fname']);
 
-$mail->Subject    = "feedback from zeneronlabs";
+// $mail->Subject    = "feedback from zeneronlabs";
 
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+// $mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
 
-$mail->MsgHTML($body);
+// $mail->MsgHTML($body);
 
-$address = "whoto@otherdomain.com";
-$mail->AddAddress($address, "John Doe");
+// $address = "whoto@otherdomain.com";
+// $mail->AddAddress($address, "John Doe");
 
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
-}
+// if(!$mail->Send()) {
+//   echo "Mailer Error: " . $mail->ErrorInfo;
+// } else {
+//   echo "Message sent!";
+// }
     
+
+
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "zeneronlabs@gmail.com";
+$mail->Password = "5comrapo";
+$mail->SetFrom("zeneronlabs@gmail.com");
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("zeneronlabs@gmail.com");
+ if(!$mail->Send())
+    {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else
+    {
+    echo "Message has been sent";
+    }
+
+
 
 
 // require_once "Mail.php";
